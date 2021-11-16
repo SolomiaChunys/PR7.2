82e75a428a5982233b1b13219f4bee5e8f51d84d
Solomiia <solomiia.chunys.ik.2021@lpnu.ua>
2021-11-16 21:31:12 +0200

    Add project files.

---
+++ UnitTest7.2/UnitTest7.2.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "c:\Соломія Чунис\Політехніка\Алгоритмізація та програмування\lab№7.2\PR7.2\PR7.2\PR7.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72
+{
+	TEST_CLASS(UnitTest72)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int k = 4;
+			const int n = 3;
+			int** a = new int* [k];
+			for (int i = 0; i < k; i++)
+				a[i] = new int[n];
+			int a[][3] = { {2, 9, 6},{7, 8, 10},{14, 14, 7},{6, 13, 14} };
+			int t = MinMax(a, 4, 3);
+			Assert::AreEqual(10, t);
+		}
+	};
+}
